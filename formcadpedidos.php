08de99245c763f9ad4b3d997a25a45a8e52d46a0
Lázaro <lazaro.2022315968@aluno.iffar.edu.br>
2024-11-22 16:54:44 -0300

    aaa

--- formcadpedidos.php
+++ formcadpedidos.php
@@ -14,7 +14,33 @@
     <title>CRUD JS</title>
 
 </head>
-<?php include ("headers.php"); ?>
+<?php include("headers.php"); ?>
+
+<style>
+    body {
+        display: flex;
+        min-height: 100vh;
+        flex-direction: column;
+    }
+
+    main {
+        flex: 1 0 auto;
+    }
+
+    .font-italic {
+        font-style: italic !important;
+    }
+
+    .aptor {
+        font-size: .8em;
+        color: silver;
+    }
+
+    .page-footer {
+        color: #eaeaea !important;
+        font-size: .9em;
+    }
+</style>
 
 <body>
     <main class="container">
@@ -51,19 +77,49 @@
             </form>
         </div>
 
-            <table>
-                <thead>
-                    <tr>
-                        <th>ID</th>
-                        <th>Nome</th>
-                        <th>Descricao</th>
-                        <th>Categoria</th>
-                        <th colspan="2">Opções</th>
-                    </tr>
-                </thead>
-                <tbody id="pedidos"></tbody>
-            </table>
+        <table>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Nome</th>
+                    <th>Descricao</th>
+                    <th>Categoria</th>
+                    <th colspan="2">Opções</th>
+                </tr>
+            </thead>
+            <tbody id="pedidos"></tbody>
+        </table>
     </main>
+
+    <footer class="page-footer black">
+        <div class="container">
+            <div class="row center-align">
+                <div class="col l4 s12">
+                    <h5 class="">Trabalho de tópicos</h5>
+                    <p class="font-italic aptor">Desenvolvido por Lázaro</p>
+                </div>
+                <div class="col l4 s12">
+                    <p>
+                    <h5 class="white-text">Institucional</h5>
+                    </p>
+                    <div class="grey-text text-lighten-4">Fale com nós<br></div>
+                    <div class="grey-text text-lighten-4">Quem somos<br></div>
+                    <div class="grey-text text-lighten-4">Perguntas frequentes<br></div>
+                </div>
+                <div class="col l4 s12">
+                    <h5 class="white-text">Siga-nos</h5>
+                    <img src="img/instagram.png" width="30">
+                    <img src="img/tiktok.png" width="30">
+                    <img src="img/telegram.webp" width="30">
+                </div>
+            </div>
+        </div>
+        <div class="footer-copyright">
+            <div style="text-align: center;" class="container">
+                © 2024 Copyright
+            </div>
+        </div>
+    </footer>
 
 </body>
 <script src="script.js"></script>
--- index.php
+++ index.php
@@ -12,13 +12,70 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Pedidos Inicio</title>
 </head>
-<?php include ("headers.php"); ?>
+<?php include("headers.php"); ?>
+<style>
+    body {
+        display: flex;
+        min-height: 100vh;
+        flex-direction: column;
+    }
+
+    main {
+        flex: 1 0 auto;
+    }
+
+    .font-italic {
+        font-style: italic !important;
+    }
+
+    .aptor {
+        font-size: .8em;
+        color: silver;
+    }
+
+    .page-footer {
+        color: #eaeaea !important;
+        font-size: .9em;
+    }
+</style>
+
 <body>
     <main class="container">
         <h1>Olá P. diddy</h1>
 
         <a href="formcadpedidos.php">Cadastrar pedidos</a>
     </main>
+
+    <footer class="page-footer black">
+        <div class="container">
+            <div class="row center-align">
+                <div class="col l4 s12">
+                    <h5 class="">Trabalho de tópicos</h5>
+                    <p class="font-italic aptor">Desenvolvido por Lázaro</p>
+                </div>
+                <div class="col l4 s12">
+                    <p>
+                    <h5 class="white-text">Institucional</h5>
+                    </p>
+                    <div class="grey-text text-lighten-4">Fale com nós<br></div>
+                    <div class="grey-text text-lighten-4">Quem somos<br></div>
+                    <div class="grey-text text-lighten-4">Perguntas frequentes<br></div>
+                </div>
+                <div class="col l4 s12">
+                    <h5 class="white-text">Siga-nos</h5>
+                    <img src="img/instagram.png" width="30">
+                    <img src="img/tiktok.png" width="30">
+                    <img src="img/telegram.webp" width="30">
+                </div>
+            </div>
+        </div>
+        <div class="footer-copyright">
+            <div style="text-align: center;" class="container">
+                © 2024 Copyright
+            </div>
+        </div>
+    </footer>
+
 </body>
 
 </html>
